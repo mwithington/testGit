4c6434abca5e50c00de08fa6b31f0a7d1c182ebb
mwithington <withingtonm@wit.edu>
2015-05-19 10:05:11 -0400

    Huge Push
    one thousand folders

---
+++ Responsive paterns/Responsive paterns/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("Responsive_paterns")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Hewlett-Packard Company")> 
+<Assembly: AssemblyProduct("Responsive_paterns")> 
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b009983b-0042-4308-9c67-29a131a94ca2")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ Responsive paterns/Responsive paterns/newArticle.js
@@ -0,0 +1,18 @@
+﻿app.controller("MainContoller", ["$scope",
+function ($scope) {
+    $scope.userList = [{
+        userName: 'Mwithington',
+        timeCreated: new Date(),
+        postCount: '0'
+    }
+    ]
+    $scope.addUser = function () {
+        $scope.userList.push({
+            userName: $scope.fromUserText,
+            time: new Date(),
+            postCount: '0'
+        })
+    }
+}
+
+]);
